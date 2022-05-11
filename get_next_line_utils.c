443085fee5f6d1a9eec01fd765a41d8dfd5d3f73
mycaelli <mycaelli@usp.br>
2022-05-11 10:49:51 +0200

    seg fault solution: initialize s1 if NULL

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 19:22:46 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/05/11 00:14:55 by coder            ###   ########.fr       */
+/*   Updated: 2022/05/11 10:12:11 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,21 @@ char	*ft_strjoin(char *s1, char const *s2)
 {
 	int		i;
 	int		j;
-	int		s1_len;
 	char	*concat;
 
 	i = 0;
 	j = 0;
-	s1_len = ft_strlen(s1);
-	concat = malloc((s1_len + ft_strlen(s2) + 1) * sizeof(char));
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	concat = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!concat)
 		return (NULL);
-	while (i < s1_len)
+	while (s1[i])
 	{
 		concat[i] = s1[i];
 		i++;
@@ -48,6 +53,7 @@ char	*ft_strjoin(char *s1, char const *s2)
 		j++;
 	}
 	concat[i + j] = '\0';
+	free(s1);
 	return (concat);
 }
 
@@ -63,4 +69,3 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *) s);
 	return (NULL);
 }
-
